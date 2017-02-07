88e8c39a80cf1202af95293082160ce1774167bd
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-07 16:07:19 +0530

    Add text views for showing price and method to update it

--- app/src/main/java/com/example/android/placeorder/MainActivity.java
+++ app/src/main/java/com/example/android/placeorder/MainActivity.java
@@ -5,9 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-/**
- * This app displays an order form to order coffee.
- */
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,18 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /**
-     * This method is called when the order button is clicked.
-     */
     public void submitOrder(View view) {
-        display(1);
+        display(2);
+        displayPrice(2 * 5);
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
