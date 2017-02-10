b0d39c621e929540388fe75f768efcbf8c9d6104
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-02-10 11:47:03 +0530

    Update the order summary based on the status of check box

--- app/src/main/java/com/example/android/placeorder/MainActivity.java
+++ app/src/main/java/com/example/android/placeorder/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.placeorder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void submitOrder(View view) {
         int price = calculatePrice();
-        printOrderSummary(price);
+
+        CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whipped_cream_check_box);
+        boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+
+        printOrderSummary(price, hasWhippedCream);
     }
 
     private int calculatePrice() {
@@ -38,11 +43,16 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    private void printOrderSummary(int price) {
+    private void printOrderSummary(int price, boolean hasWhippedCream) {
         String message = "Name : Rajashree Malvade\n" +
+                "Add Whipped Cream? " + hasWhippedCream + "\n" +
                 "Quantity: " + quantity + "\n" +
                 "Total: $" + price + "\nThank you!";
 
+        displayMessage(message);
+    }
+
+    private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(message);
     }
